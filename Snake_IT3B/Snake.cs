db417eeb9a8ddfbb12577ac786860132e1a22e5a
Lukáš Macháček <Machacek.Lukas@stud.vassboskovice.cz>
2021-10-26 13:40:32 +0200

    Dodělávky

--- Snake_IT3B/Snake.cs
+++ Snake_IT3B/Snake.cs
@@ -1,13 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace Snake_IT3B
 {
-    class Snake
+    public class Snake
     {
+        List<PointF> Tail {get; set;}
+        public PointF Head {get; set;}
 
+        public double Speed {get; set;}
+        public double Direction {get; set;}
 
+        public Snake()
+        {
+            Head = new PointF();
+            Tail = new List<PointF>();
+        }
 
+        public void Forward()
+        {
+            Tail.Add(Head);
+            
+            var dx = Speed * Math.Cos(Direction);
+            var dy = Speed * -Math.Sin(Direction);
+            Head = new PointF(Convert.ToSingle(Tail[Tail.Count-1].X +dx), Convert.ToSingle(Tail[Tail.Count-1].Y + dy));
+ 
+        }
+        public void Left()
+        {
+            Direction += 0.3;
+        }
+        public void Right()
+        {
+            Direction -= 0.3;
+        }
+
+        public List<PictureBox> GetPictureBoxes(Color color, int size)
+        {
+            var halfSize = size / 2F;
+            List<PictureBox> pictureBoxes = new List<PictureBox>();
+            foreach(var point in Tail)
+            {
+                
+                pictureBoxes.Add(new PictureBox() { Location = new Point(), BackColor = color});
+            }
+            return pictureBoxes;
+        }
     }
 }
